8236f223792f0fa9d9967292ef5372a609e756a5
meeieky <meeieksmikuwa@gmail.com>
2023-08-17 20:31:46 +0200

    is

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @x: The number to be checked
+ *
+ * Return: 0 0r 1
+ */
+
+int _isupper(int x)
+
+{
+	
+	if (x >= 'A' && x <= 'Z')
+	{
+		return (1);
+	}
+		return (0);
+}
