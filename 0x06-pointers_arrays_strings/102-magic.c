f6216cd959227b9abbc6b497b32443ea6cd09f8d
meeieky <meeieksmikuwa@gmail.com>
2023-08-24 17:40:58 +0200

    kol

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,6 @@ int main(void)
 	int a[5];
 	int *p;
 
-
 	a[2] = 1024;
 	p = &n;
 	/*
@@ -15,7 +14,7 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 *   - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
