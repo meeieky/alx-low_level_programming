59b69434ca90fa72ff55e1b25940501500efb15b
meeieky <meeieksmikuwa@gmail.com>
2023-08-31 23:01:22 +0200

    kolo

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+void _putchar(char c);
 void _puts_recursion(char *s);
-int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,5 +12,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* MAIN_H */
