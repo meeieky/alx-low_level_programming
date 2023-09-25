825fb36f870a5ce43d988290e4c2a8e755f332bc
meeieky <meeieksmikuwa@gmail.com>
2023-09-25 14:41:01 +0200

    kdh

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - Frees a listint_t list.
+ * @h: Free lists with a loop
+ * Return: Size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
