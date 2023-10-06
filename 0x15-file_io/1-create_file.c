329a1b0865109474b9efdba9d9e973f28db3d18d
meeieky <meeieksmikuwa@gmail.com>
2023-10-06 07:48:14 +0200

    hoils

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * create_file - Function that creates a file
  * @filename: name of the file to create
  * @text_content: is a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
